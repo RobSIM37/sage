2df673c2bd5f34dbb7f6de49940552316d34f7c5
Rob Lewis <robsim37@gmail.com>
2025-03-05 15:53:07 -0500

    rem node node_modules

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloSAGE = void 0;
+const helloSAGE = () => {
+    return "SAGE is alive!";
+};
+exports.helloSAGE = helloSAGE;
+console.log((0, exports.helloSAGE)());
