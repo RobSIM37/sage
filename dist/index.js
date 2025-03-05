0b1c0a26fd959220d7a49bc94b65b39ab6dfbc20
Robert Lewis <92329939+RobSIM37@users.noreply.github.com>
2025-03-05 15:45:54 -0500

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloSAGE = void 0;
-const helloSAGE = () => {
-    return "SAGE is alive!";
-};
-exports.helloSAGE = helloSAGE;
-console.log((0, exports.helloSAGE)());
